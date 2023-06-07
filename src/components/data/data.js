a144ac17da29fcc380895739091679d1767ebe61
shorif <sashipon001@gmail.com>
2023-06-07 15:57:02 +0600

    react tailwind responsive home page

---
+++ src/components/data/data.js
@@ -0,0 +1,99 @@
+export const data = [
+  {
+    id: 1,
+    name: "Double Cheeseburger",
+    Category: "burger",
+    image: "https://images.unsplash.com/photo-1538991383142-36c4edeaffde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$$"
+  },
+  {
+    id: 2,
+    name: "Uttara Cheeseburger",
+    Category: "pizza",
+    image: "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$$"
+  },
+  {
+    id: 3,
+    name: "Gazipur Cheeseburger",
+    Category: "salad",
+    image: "https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$$"
+  },
+  {
+    id: 4,
+    name: "Savar Cheeseburger",
+    Category: "chicken",
+    image: "https://images.unsplash.com/photo-1532983330958-4b32bbe9bb0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyZWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$"
+  },
+  {
+    id: 5,
+    name: "Azompur Cheeseburger",
+    Category: "burger",
+    image: "https://images.unsplash.com/photo-1511553677255-ba939e5537e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZyZWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$"
+  },
+  {
+    id: 6,
+    name: "Khilkhet Cheeseburger",
+    Category: "pizza",
+    image: "https://images.unsplash.com/photo-1504466334719-af4ae9f12ad0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bmlnaHQlMjBtb29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    price: "$$$"
+  },
+  {
+    id: 7,
+    name: "Bashundhara Cheeseburger",
+    Category: "burger",
+    image: "https://images.unsplash.com/photo-1568420162086-b16456b8d557?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmlnaHQlMjBtb29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    price: "$$"
+  },
+  {
+    id: 8,
+    name: "Rampura Cheeseburger",
+    Category: "salad",
+    image: "https://images.unsplash.com/photo-1536096736400-07fc9cc777df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fG5pZ2h0JTIwbW9vbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$"
+  },
+  {
+    id: 9,
+    name: "Mirpur Cheeseburger",
+    Category: "burger",
+    image: "https://images.unsplash.com/photo-1552658191-c52a26cfb726?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjU2fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$"
+  },
+  {
+    id: 10,
+    name: "Dhaka Cheeseburger",
+    Category: "chicken",
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    price: "$$$"
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    name: 'Fast Food',
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    name: 'pizza',
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    name: 'Wings',
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    name: 'Latest Deals',
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    name: 'Fast Food',
+    image: "https://images.unsplash.com/photo-1479762937580-3b682a10a0d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg0fHxuaWdodCUyMG1vb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+  },
+]
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; 
+
+// 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
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ 
